cb7d470b5e572aa1e680c58fd0cb46f92b156de1
dariaCusutura <cusuturadaria@gmail.com>
2024-05-21 08:48:52 +0300

    Created create-user dto

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,5 +1,26 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
+  @MaxLength(50)
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(30)
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(30)
+  @IsNotEmpty()
   password: string;
 }
